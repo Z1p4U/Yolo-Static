680fc8f83632707f92e93c7183707ec8a8966a28
phyothantzin <phyothantzin.w@gmail.com>
2024-02-08 14:03:05 +0630

    ...

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,7 @@
 "use client";
-
-import React, { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import { Link } from "react-scroll";
 import Navbar from "./nav/Navbar";
 
 const Header = () => {
-  const [active, setActive] = useState("");
-  const [open, setOpen] = useState(false);
-
   return <Navbar visibleDefault />;
 };
 
